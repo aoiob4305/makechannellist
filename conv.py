4632979ffbf4cc1403bf5f16dfb22057a99eb47e
aoiob4305 <aoiob4305@gmail.com>
2017-05-29 00:59:38 +0900

    첫줄에 필드이름이 들어가는 문제 수정

--- conv.py
+++ conv.py
@@ -6,6 +6,6 @@ with open('skChannels.csv', newline='',encoding='utf8') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
     print('#EXTM3U')
     for row in spamreader:
-        if row[3] != '':
+        if row[3] != '' and row[3] != 'ip':
             print('#EXTINF:-1 tvg-id="%s" tvg-chnum="%s",%s\nudp://%s:%s' % (row[0],row[2],row[1],row[3],row[4]))
 
